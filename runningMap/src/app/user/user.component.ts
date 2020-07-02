99ac7260162105e4a917944cc641799bf54507bf
caroline-ctrl <duon.caroline@gmail.com>
2020-07-02 12:04:15 +0200

    implement user.component methode getById

--- runningMap/src/app/user/user.component.ts
+++ runningMap/src/app/user/user.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: [ './user.component.css' ]
 })
 export class UserComponent implements OnInit {
+  private currentUser = null;
 
-  constructor() { }
+  constructor(
+    private serviceUser: UserService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.getById(this.route.snapshot.paramMap.get('id'));
   }
 
+  // id sera ensuite en fonction de la session
+  getById(id) {
+    this.serviceUser.getUserById(id).subscribe(
+      (user) => {
+        this.currentUser = user;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
