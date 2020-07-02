ee6dad58e477f7747a9faf1c0e09f6f9b40c6f37
caroline-ctrl <duon.caroline@gmail.com>
2020-07-02 11:40:53 +0200

    creation requete getUserById dans service

--- runningMap/src/app/user/user.service.ts
+++ runningMap/src/app/user/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private URL_API = 'mongodb+srv://carod:clemL34270+@cluster0-fbriu.gcp.mongodb.net/running_map?retryWrites=true&w=majority';
+
+  constructor(private http: HttpClient) { }
+
+  getUserById(id: string) {
+    return this.http.get<User>(this.URL_API + id);
+  }
 }
