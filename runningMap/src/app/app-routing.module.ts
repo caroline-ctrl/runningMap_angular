764e833ce71b0a5904efd9134faf06a64d27b481
caroline-ctrl <duon.caroline@gmail.com>
2020-07-03 10:31:17 +0200

    creation de la route

--- runningMap/src/app/app-routing.module.ts
+++ runningMap/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserCreateComponent } from './user/user-create/user-create.component';
+import { UserUpdateComponent } from './user/user-update/user-update.component';
 
 
 const routes: Routes = [
+  { path: 'update/:id', component: UserUpdateComponent},
   { path: 'user/:id', component: UserDetailComponent },
   { path: 'create', component: UserCreateComponent},
   { path: 'users', component: UserComponent}
