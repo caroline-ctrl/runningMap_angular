acab91680cd31e2ffbf232cf2ca84c2be993a90b
caroline-ctrl <duon.caroline@gmail.com>
2020-07-03 10:53:46 +0200

    implement route

--- runningMap/src/app/app-routing.module.ts
+++ runningMap/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { UserComponent } from './user/user.component';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserCreateComponent } from './user/user-create/user-create.component';
 import { UserUpdateComponent } from './user/user-update/user-update.component';
+import { UserArchiveComponent } from './user/user-archive/user-archive.component';
 
 
 const routes: Routes = [
+  { path: 'archiv/:id', component: UserArchiveComponent},
   { path: 'update/:id', component: UserUpdateComponent},
   { path: 'user/:id', component: UserDetailComponent },
   { path: 'create', component: UserCreateComponent},
