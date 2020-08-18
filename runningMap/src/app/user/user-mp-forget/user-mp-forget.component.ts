25bce251d96f395d7c9a2234b2b0716b407f6d84
caroline-ctrl <duon.caroline@gmail.com>
2020-08-18 15:35:51 +0200

    ajout commentaire user-mp-forget

--- runningMap/src/app/user/user-mp-forget/user-mp-forget.component.ts
+++ runningMap/src/app/user/user-mp-forget/user-mp-forget.component.ts
@@ -28,14 +28,20 @@ export class UserMpForgetComponent implements OnInit {
     });
   }
 
-  sendMail() {
+  // envoie de mail
+  sendMail(): void {
+    // recupère la valeur de l'input du formulaire
     this.mail = this.user.value.mail;
 
     this.userService.mail(this.mail).subscribe((user) => {
+      // permet d'obtenir la longueur de l'objet
       if(Object.keys(user).length > 0) {
         alert('Un mail vient de vous être envoyé');
-        this.messageCode = prompt('Renseigner votre code envoyé par mail'); // messageCode contient le code envoyé par le user
+        // messageCode contient le code envoyé par le user
+        this.messageCode = prompt('Renseigner votre code envoyé par mail');
+        // verifie si le code est identique entre la bdd et celui renseigné
         this.verifyCode(this.messageCode);
+        // si l'objet est vide
       } else if (Object.keys(user).length === 0){
         alert('L\'adresse mail n\'existe pas');
       }
@@ -44,11 +50,14 @@ export class UserMpForgetComponent implements OnInit {
     });
   }
 
-  verifyCode(code) {
+  // verifie le code de sécurité
+  verifyCode(code): void {
     this.userService.verifyCode(this.mail).subscribe((user) => {
       this.currentUser = user;
       if (code === this.currentUser.token){
+        // redirige vers la page password
         this.router.navigate(['index/password']);
+        // met le code de sécurité en cookie
         this.cookieService.set('token', code, 7, 'http://localhost:3000', '', false, 'Lax');
       } else {
         alert('Le code n\est pas bon');
